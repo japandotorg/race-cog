5bf411e3ffe3828423762289f60bcb164b9e4f08
Lemon Rose <yash.kul69@gmail.com>
2023-02-01 01:26:31 +0530

    grammer

--- race/race.py
+++ race/race.py
@@ -110,7 +110,7 @@ class Race(commands.Cog):
         await asyncio.sleep(wait)
         self.started[ctx.guild.id] = True 
         if len(self.players[ctx.guild.id]) <= 2:
-            return await ctx.send("At least need two or more players to start a race!")
+            return await ctx.send("At least two or more players are needed to start a race!")
         else:   
             await ctx.send("<a:raceprocess:892002710843428864> **__THE RACE IS NOW IN PROGRESS__** <a:raceprocess:892002710843428864>")
             await self.run_game(ctx)
